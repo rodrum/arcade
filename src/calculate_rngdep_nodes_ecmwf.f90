dbca7ab08df107db8892cea087627c3491d1610d
Rodrigo De Negri <rsd00@ucsb.edu>
2022-11-22 10:56:06 -0800

    Finalized the discretization including seconds

--- src/calculate_rngdep_nodes_ecmwf.f90
+++ src/calculate_rngdep_nodes_ecmwf.f90
@@ -7,7 +7,7 @@ implicit none
 integer :: nrec
 integer :: num_sou
 integer :: num_sta
-integer :: num_doy
+integer :: num_doy, num_sec
 integer :: num_lat
 integer :: num_lon
 ! to deal with NRLMSIS2.0 input/output
@@ -23,16 +23,27 @@ real(4) :: sec, alt, glat, glon, stl, f107a, f107
 ! to deal with ECMWF values
 real(4) :: ecmwf_h, ecmwf_t, ecmwf_zonw, ecmwf_merw
 
-integer, dimension(:), allocatable :: doy_vec
+integer, dimension(:), allocatable :: doy_vec, sec_vec
 
 ! dummy variables
-integer :: idoy, isou, ista, ilin, ilat, ilon
+integer :: idoy, isou, ista, ilin, ilat, ilon, isec
 integer :: OpenStatus
 
 character(100) :: nodes_in, nodes_out, ecmwf_in
 
 !Initialize NRMLSIS2.0 model
 call msisinit(parmpath='./',parmfile='msis20.parm')
+
+write(*,*) "Reading ", "../input/secs.txt"
+open (10, FILE="../input/secs.txt", STATUS="OLD", ACTION="read",        &
+      POSITION="rewind", IOSTAT=OpenStatus)
+if (OpenStatus > 0) STOP "*** Cannot open file ***"
+num_sec = Count_Lines(10)
+allocate(sec_vec(num_sec))
+do ilin = 1, num_sec
+    read(10, '(I3)') sec_vec(ilin)
+enddo
+close(10)
 
 write(*,*) "Reading ", "../input/doys.txt"
 open (10, FILE="../input/doys.txt", STATUS="OLD", ACTION="read",        &
@@ -75,135 +86,143 @@ close(10)
 
 write(*,*) num_doy, num_sou, num_sta, num_lat, num_lon
 ! Input/output path+file name of each nodes file
-100 format (A28, I3.3, A1, I5.5, A1, I4.4, A1, I4.4, A1, I4.4, A4)
-200 format (A27, I3.3, A1, I5.5, A1, I4.4, A1, I4.4, A1, I4.4, A4)
+100 format (A28, I5.5, A1, I3.3, A1, I5.5, A1, I4.4, A1, I4.4, A1, I4.4, A4)
+200 format (A27, I5.5, A1, I3.3, A1, I5.5, A1, I4.4, A1, I4.4, A1, I4.4, A4)
 
-do idoy = 1, size(doy_vec)
-    do isou = 1, num_sou
-        do ista = 1, num_sta
-            do ilat = 1, num_lat
-                do ilon = 1, num_lon
-                    !=== climatologies
-                    write(nodes_in, 100) &
-                        "../output/nodes/nodes_climt_", &
-                        doy_vec(idoy), "_", &
-                        isou, "_", &
-                        ista, "_", &
-                        ilat, "_", &
-                        ilon, ".txt"
-                    write(*,*) "Reading ", trim(nodes_in)
-                    open (10, FILE=trim(nodes_in), STATUS="OLD", ACTION="read",        &
-                        POSITION="rewind", IOSTAT=OpenStatus)
-                    if (OpenStatus > 0) STOP "*** Cannot open file ***"
+do isec = 1, size(sec_vec)
+    do idoy = 1, size(doy_vec)
+        do isou = 1, num_sou
+            do ista = 1, num_sta
+                do ilat = 1, num_lat
+                    do ilon = 1, num_lon
+                        !=== climatologies
+                        write(nodes_in, 100) &
+                            "../output/nodes/nodes_climt_", &
+                            sec_vec(isec), "_", &
+                            doy_vec(idoy), "_", &
+                            isou, "_", &
+                            ista, "_", &
+                            ilat, "_", &
+                            ilon, ".txt"
+                        write(*,*) "Reading ", trim(nodes_in)
+                        open (10, FILE=trim(nodes_in), STATUS="OLD", ACTION="read",        &
+                            POSITION="rewind", IOSTAT=OpenStatus)
+                        if (OpenStatus > 0) STOP "*** Cannot open file ***"
 
-                    nrec = Count_Lines(10)
-                    write(*,*) nrec
-                    write(nodes_out, 100) &
-                    "../output/nodes/descr_climt_", &
-                    doy_vec(idoy), &
-                    "_", isou, &
-                    "_", ista, &
-                    "_", ilat, &
-                    "_", ilon, &
-                    ".txt"
+                        nrec = Count_Lines(10)
+                        write(*,*) nrec
+                        write(nodes_out, 100) &
+                            "../output/nodes/descr_climt_", &
+                            sec_vec(isec), &
+                            doy_vec(idoy), &
+                            "_", isou, &
+                            "_", ista, &
+                            "_", ilat, &
+                            "_", ilon, &
+                            ".txt"
 
-                    ! Open file to save profiles with name created above
-                    open (20, FILE=TRIM(nodes_out), STATUS="REPLACE",              &
-                        ACTION="write", POSITION="rewind", IOSTAT=OpenStatus)
-                    if (OpenStatus > 0) STOP "*** Cannot open file ***"
-                    write(*,*) "    Writing to ", trim(nodes_out)
+                        ! Open file to save profiles with name created above
+                        open (20, FILE=TRIM(nodes_out), STATUS="REPLACE",              &
+                            ACTION="write", POSITION="rewind", IOSTAT=OpenStatus)
+                        if (OpenStatus > 0) STOP "*** Cannot open file ***"
+                        write(*,*) "    Writing to ", trim(nodes_out)
 
-                    do ilin = 1, nrec
-                        read(10,*) iyd,sec,alt,glat,glon,stl,f107a,f107,apd,aph
-                        ap_msis(1) = apd ! daily ap
-                        ap_hwm(2) = aph ! 3h ap indesx for 3 hrs before current time (fix this)
-                        ! from checkwhm14.f90, not really used but maybe...
-                        stl = pershift(sec + glon/15.0, (/0.0, 24.0/) )
-                        call gtd8d(iyd,sec,alt,glat,glon,stl,f107a,f107,ap_msis,mass,d,t)
-                        call hwm14(iyd,sec,alt,glat,glon,stl,f107a,f107,ap_hwm,w)
-                        !write(20,'(2i7,3f7.1,e13.4,f8.2,2f9.3)')  &
-                        !iyd,int(sec),alt,glat,glon,d(6),t(2),w(1),w(2)
-                        write(20,'(5e13.4)')  &
-                        alt,d(6),t(2),w(1),w(2)
-                    enddo
-                    close(20) ! nodes output file
-                    write(*,*) "    Done writing."
-                    close(10) ! nodes input file
-                    write(*,*) "Done reading."
-
-                    !=== ECMWF ERA 5 nodes to complete with HWM14 and MSIS2.0
-                    write(nodes_in, 100) &
-                        "../output/nodes/nodes_ecmwf_", &
-                        doy_vec(idoy), "_", &
-                        isou, "_", &
-                        ista, "_", &
-                        ilat, "_", &
-                        ilon, ".txt"
-                    write(*,*) "Reading ", trim(nodes_in)
-                    open (10, FILE=trim(nodes_in), STATUS="OLD", ACTION="read",        &
-                        POSITION="rewind", IOSTAT=OpenStatus)
-                    if (OpenStatus > 0) STOP "*** Cannot open file ***"
-
-                    nrec = Count_Lines(10)
-                    write(*,*) nrec
-                    
-                    !=== interpolated temp, zonw, and merw, values at nodes from ECMWF
-                    write(ecmwf_in, 200) &
-                        "../output/ecmwf/vals_ecmwf_", &
-                        doy_vec(idoy), "_", &
-                        isou, "_", &
-                        ista, "_", &
-                        ilat, "_", &
-                        ilon, ".dat"
-
-                    write(*,*) "Reading ", trim(ecmwf_in)
-                    open (30, FILE=trim(ecmwf_in), STATUS="OLD", ACTION="read",        &
-                    POSITION="rewind", IOSTAT=OpenStatus)
-                    if (OpenStatus > 0) STOP "*** Cannot open file ***"
-
-                    !num_lin = Count_Lines(30)
-                    read(30,*) ecmwf_h, ecmwf_t, ecmwf_zonw, ecmwf_merw 
-
-                    write(nodes_out, 100) &
-                        "../output/nodes/descr_ecmwf_", &
-                        doy_vec(idoy), &
-                        "_", isou, &
-                        "_", ista, &
-                        "_", ilat, &
-                        "_", ilon, &
-                        ".txt"
-
-                    ! Open file to save profiles with name created above
-                    open (20, FILE=TRIM(nodes_out), STATUS="REPLACE",              &
-                        ACTION="write", POSITION="rewind", IOSTAT=OpenStatus)
-                    if (OpenStatus > 0) STOP "*** Cannot open file ***"
-                    write(*,*) "    Writing to ", trim(nodes_out)
-
-                    do ilin = 1, nrec
-                        read(10,*) iyd,sec,alt,glat,glon,stl,f107a,f107,apd,aph
-                        ap_msis(1) = apd ! daily ap
-                        ap_hwm(2) = aph ! 3h ap indesx for 3 hrs before current time (fix this)
-                        ! from checkwhm14.f90, not really used but maybe...
-                        stl = pershift(sec + glon/15.0, (/0.0, 24.0/) )
-                        call gtd8d(iyd,sec,alt,glat,glon,stl,f107a,f107,ap_msis,mass,d,t)
-                        if (alt <= ecmwf_h) then
-                            !write(20,'(2i7,f9.4,2f7.1,e13.4,f8.2,2f9.3)')  &
+                        do ilin = 1, nrec
+                            read(10,*) iyd,sec,alt,glat,glon,stl,f107a,f107,apd,aph
+                            ap_msis(1) = apd ! daily ap
+                            ap_hwm(2) = aph ! 3h ap indesx for 3 hrs before current time (fix this)
+                            ! from checkwhm14.f90, not really used but maybe...
+                            stl = pershift(sec + glon/15.0, (/0.0, 24.0/) )
+                            call gtd8d(iyd,sec,alt,glat,glon,stl,f107a,f107,ap_msis,mass,d,t)
+                            call hwm14(iyd,sec,alt,glat,glon,stl,f107a,f107,ap_hwm,w)
+                            !write(20,'(2i7,3f7.1,e13.4,f8.2,2f9.3)')  &
+                            !iyd,int(sec),alt,glat,glon,d(6),t(2),w(1),w(2)
                             write(20,'(5e13.4)')  &
-                            ecmwf_h,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
-                            !iyd,int(sec),ecmwf_h,glat,glon,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
-                        else
-                            read(30,*) ecmwf_h, ecmwf_t, ecmwf_zonw, ecmwf_merw 
-                            !write(20,'(2i7,f9.4,2f7.1,e13.4,f8.2,2f9.3)')  &
-                            write(20,'(5e13.4)')  &
-                            ecmwf_h,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
-                            !iyd,int(sec),ecmwf_h,glat,glon,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
-                        endif
+                            alt,d(6),t(2),w(1),w(2)
+                        enddo
+                        close(20) ! nodes output file
+                        write(*,*) "    Done writing."
+                        close(10) ! nodes input file
+                        write(*,*) "Done reading."
+
+                        !=== ECMWF ERA 5 nodes to complete with HWM14 and MSIS2.0
+                        write(nodes_in, 100) &
+                            "../output/nodes/nodes_ecmwf_", &
+                            sec_vec(isec), "_", &
+                            doy_vec(idoy), "_", &
+                            isou, "_", &
+                            ista, "_", &
+                            ilat, "_", &
+                            ilon, ".txt"
+                        write(*,*) "Reading ", trim(nodes_in)
+                        open (10, FILE=trim(nodes_in), STATUS="OLD", ACTION="read",        &
+                            POSITION="rewind", IOSTAT=OpenStatus)
+                        if (OpenStatus > 0) STOP "*** Cannot open file ***"
+
+                        nrec = Count_Lines(10)
+                        write(*,*) nrec
+                        
+                        !=== interpolated temp, zonw, and merw, values at nodes from ECMWF
+                        write(ecmwf_in, 200) &
+                            "../output/ecmwf/vals_ecmwf_", &
+                            sec_vec(isec), "_", &
+                            doy_vec(idoy), "_", &
+                            isou, "_", &
+                            ista, "_", &
+                            ilat, "_", &
+                            ilon, ".dat"
+
+                        write(*,*) "Reading ", trim(ecmwf_in)
+                        open (30, FILE=trim(ecmwf_in), STATUS="OLD", ACTION="read",        &
+                        POSITION="rewind", IOSTAT=OpenStatus)
+                        if (OpenStatus > 0) STOP "*** Cannot open file ***"
+
+                        !num_lin = Count_Lines(30)
+                        read(30,*) ecmwf_h, ecmwf_t, ecmwf_zonw, ecmwf_merw 
+
+                        write(nodes_out, 100) &
+                            "../output/nodes/descr_ecmwf_", &
+                            sec_vec(isec), &
+                            doy_vec(idoy), &
+                            "_", isou, &
+                            "_", ista, &
+                            "_", ilat, &
+                            "_", ilon, &
+                            ".txt"
+
+                        ! Open file to save profiles with name created above
+                        open (20, FILE=TRIM(nodes_out), STATUS="REPLACE",   &
+                            ACTION="write", POSITION="rewind",              &
+                            IOSTAT=OpenStatus)
+                        if (OpenStatus > 0) STOP "*** Cannot open file ***"
+                        write(*,*) "    Writing to ", trim(nodes_out)
+
+                        do ilin = 1, nrec
+                            read(10,*) iyd,sec,alt,glat,glon,stl,f107a,f107,apd,aph
+                            ap_msis(1) = apd ! daily ap
+                            ap_hwm(2) = aph ! 3h ap indesx for 3 hrs before current time (fix this)
+                            ! from checkwhm14.f90, not really used but maybe...
+                            stl = pershift(sec + glon/15.0, (/0.0, 24.0/) )
+                            call gtd8d(iyd,sec,alt,glat,glon,stl,f107a,f107,ap_msis,mass,d,t)
+                            if (alt <= ecmwf_h) then
+                                !write(20,'(2i7,f9.4,2f7.1,e13.4,f8.2,2f9.3)')  &
+                                write(20,'(5e13.4)')  &
+                                ecmwf_h,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
+                                !iyd,int(sec),ecmwf_h,glat,glon,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
+                            else
+                                read(30,*) ecmwf_h, ecmwf_t, ecmwf_zonw, ecmwf_merw 
+                                !write(20,'(2i7,f9.4,2f7.1,e13.4,f8.2,2f9.3)')  &
+                                write(20,'(5e13.4)')  &
+                                ecmwf_h,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
+                                !iyd,int(sec),ecmwf_h,glat,glon,d(6),ecmwf_t,ecmwf_merw,ecmwf_zonw
+                            endif
+                        enddo
+                        close(20) ! nodes output file
+                        write(*,*) "    Done writing."
+                        close(10) ! nodes input file
+                        close(30)
+                        write(*,*) "Done reading."
                     enddo
-                    close(20) ! nodes output file
-                    write(*,*) "    Done writing."
-                    close(10) ! nodes input file
-                    close(30)
-                    write(*,*) "Done reading."
                 enddo
             enddo
         enddo
--- src/discretize.py
+++ src/discretize.py
@@ -393,7 +393,6 @@ def rng_dep_ecmwf():
     print("../output/profiles/nodes-lat.loc saved.")
 
     sec_doy_sou_sta = []
-    #doy =  params['doys'][0] # NOTE: only one day implemented for now 
     #=== get fixed altitudes from levels file (taken from 'interpolate_ecmwf.py')
     # load levels 1/to/137
     levels_file = join("../input", 'ECMWF - L137 model level definitions.csv')
